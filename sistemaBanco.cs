985318f3a88b758860bc90b81e0a0694a2b7a3d9
Samuel Luiz Souza <samuelluizsouza771@gmail.com>
2024-12-21 21:54:15 -0300

    coloquei os arquivos que usei no projeto

---
+++ sistemaBanco.cs
@@ -0,0 +1,17 @@
+﻿namespace testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Eu gosto de C# e programação.";
+            int indice = texto.IndexOf("gosto"); // Retorna 3
+            int indic = texto.IndexOf("Java"); // Retorna -1
+            int indi = texto.IndexOf("c#", StringComparison.OrdinalIgnoreCase); // Retorna 11
+
+            Console.WriteLine(indice);
+            Console.WriteLine(indic);
+            Console.WriteLine(indi);
+        }
+    }
+}
