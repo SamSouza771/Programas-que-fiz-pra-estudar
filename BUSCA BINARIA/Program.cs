51a0d00a2c48ae4225fa5d06a27c998f3f0ce744
Samuel Luiz Souza <samuelluizsouza771@gmail.com>
2024-12-26 22:41:39 -0300

    adicionei os arquivos as suas respectivas pastas

---
+++ BUSCA BINARIA/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace binarysearch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] lista = new int[] { 4, 7, 14, 25, 33, 41, 48, 51, 67, 70, 72, 75, 77, 79, 80, 81, 91, 94, 95, 98 };
+
+            Console.WriteLine(Busca(48, lista));
+            Console.WriteLine(Busca(98, lista));
+            Console.WriteLine(Busca(79, lista));
+            Console.WriteLine(Busca(215, lista));
+            Console.WriteLine(Busca(51, lista));
+            Console.WriteLine(Busca(25, lista));
+        }
+
+        public static int Busca(int target, int[] lista)
+        {
+            int head = 0;
+            int tail = lista.Length - 1;
+            int pivot;
+
+            while (head <= tail)
+            {
+                pivot = head + tail / 2;
+
+                Console.WriteLine("pivot: " + pivot);
+                Console.WriteLine("head: " + head);
+                Console.WriteLine("tail: " + tail);
+                Console.WriteLine("--------------------");
+
+                if (lista[pivot] > target)
+                {
+                    tail = pivot - 1;
+                }
+                else if (lista[pivot] < target)
+                {
+                    head = pivot + 1;
+                }
+                else
+                {
+                    return lista[pivot];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
