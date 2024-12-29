8262b3a0c84e1e69939303a8d1a2f6bb676b9f8b
Samuel Luiz Souza <samuelluizsouza771@gmail.com>
2024-12-29 02:41:53 -0300

    fiz esse leet code simples, provavelmente não está muito eficiente

---
+++ LEET CODE/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public class Solution {
+    public static void Main(String[] args)
+    {
+        Console.WriteLine(IsPalindrome(121));
+        Console.WriteLine(IsPalindrome(-121));
+        Console.WriteLine(IsPalindrome(11));
+        Console.WriteLine(IsPalindrome(12));
+    }
+    public static bool IsPalindrome(int x) {
+        string strX = Convert.ToString(x);
+        char[] strZ = strX.ToCharArray();
+        Array.Reverse(strZ);
+        string inverted = new string(strZ);
+
+        if (inverted == strX)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
